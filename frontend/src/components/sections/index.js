d331cf8c8e27f0b07c24dda16ad0b9f295f33f30
Teddy <truongteddy306@protonmail.com>
2025-11-26 20:20:08 -0500

    Revert "feat: ajout navigation articles + fix race condition"
    This reverts commit aa4899a5bd7b763c785dde9a5ab21bbec6d7f161.

--- frontend/src/components/sections/index.js
+++
@@ -1,4 +0,0 @@
-export { default as ExperienceCard } from './ExperienceCard';
-export { default as ProjectCard } from './ProjectCard';
-export { default as ArticleCard } from './ArticleCard';
-export { default as SkillsBox } from './SkillsBox';
--- frontend/src/pages/index.js
+++
@@ -1,3 +0,0 @@
-export { default as HomePage } from './HomePage';
-export { default as BlogPage } from './BlogPage';
-export { default as ArticlePage } from './ArticlePage';
